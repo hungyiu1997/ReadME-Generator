087c3e3881df72cb57d43819f37740e4a715c157
Hungyiu Wong <hungyiuwong@Hungyius-MacBook-Air.local>
2021-09-02 15:25:47 -0400

    first draft

--- asset/script.js/script.js
+++ asset/script.js/script.js
@@ -1,54 +1,72 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require('util');
+const path = require('path');
 
-const generateReadMe = (answers) =>
+const writeToFile = util.promisify(fs.writeFile);
 
-inquirer
-.prompt ([
+const questions = () => {
+  return inquirer.prompt([
     {
-        type: "input",
-        name: "Project Title",
-        message: "What is the name of your project?"
+      type: "input",
+      name: "Project Title",
+      message: "What is the name of your project?",
     },
     {
-        type: "input",
-        name: "description",
-        message: "What is the project discription?"
+      type: "input",
+      name: "description",
+      message: "What is the project discription?",
     },
     {
-        type: "input",
-        name: "installation instructions",
-        message: "How should I install this?"
+      type: "input",
+      name: "installation instructions",
+      message: "How should I install this?",
     },
     {
-        type: "input",
-        name: "usuage information",
-        message: "How should I use this?"
+      type: "input",
+      name: "usuage information",
+      message: "How should I use this?",
     },
     {
-        type: "input",
-        name: "contribution guidelines",
-        message: "How can I contribute to this project?"
+      type: "input",
+      name: "contribution guidelines",
+      message: "How can I contribute to this project?",
     },
     {
-        type: "input",
-        name: "test instructions",
-        message: "How can I test this?"
+      type: "input",
+      name: "test instructions",
+      message: "How can I test this?",
     },
     {
-        type: "list",
-        name: "license",
-        message: "Choose your license",
-        choices: [""]
+      type: "list",
+      name: "license",
+      message: "Choose your license",
+      choices: [""],
     },
     {
-        type: "input",
-        name: "gitHub",
-        message: "What is your gitHub profile?"
+      type: "input",
+      name: "gitHub",
+      message: "What is your gitHub profile?",
     },
     {
-        type: "input",
-        name: "questions",
-        message: "If you have any "
+      type: "input",
+      name: "questions",
+      message: "If you have any ",
     },
-]) 
+  ]);
+};
+
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+};
+
+const generateReadMe = (questions)
+
+const init = () => {
+    promptUser ()
+        .then((answers) = writeToFile('readMe.md', generateReadMe(answers)))
+        .then(() => console.log('Successfully generated readme.md'))
+        .catch((err) => console.error(err));
+};
+
+init();
